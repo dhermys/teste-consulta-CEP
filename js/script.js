7f5a068393530ebf9b03ff90d85808cf0d6a4dd9
Diego <diegohermys@gmail.com>
2023-03-03 21:34:41 -0300

    código consertado

--- js/script.js
+++ js/script.js
@@ -12,12 +12,12 @@ function compararEstados() {
 }
 
 async function pesquisarCep() {
-    let cepConsultado = document.getElementById('cep-origem').value;
-    var validacep = /^[0-9]{8}$/;
-    if (cepConsultado.trim() === "" || !validacep.test(cepConsultado)) {
+    let cep = document.getElementById('cep-origem').value;
+    var validaCep = /^[0-9]{8}$/;
+    if (!validaCep.test(cep)) {
         document.getElementById('resposta').innerHTML = `<h5>O formato do CEP é inválido</h5>`
     } else {
-        await fetch(`https://viacep.com.br/ws/${cepConsultado}/json/`, {
+        await fetch(`https://viacep.com.br/ws/${cep}/json/`, {
             method: 'GET',
             headers: {
                 'Content-Type': 'application/json'
@@ -31,20 +31,20 @@ async function pesquisarCep() {
             if ("erro" in conteudo) {
                 document.getElementById('resposta').innerHTML = `<h5>CEP não encontrado</h5>`
             } else {
-                //estados.unshift(conteudo.uf)
+                estados.unshift(conteudo.uf)
                 
 
-                document.getElementById('resposta').innerHTML =
-                    `<div class="card" style="width: 18rem;">
-                        <div class="card-body">
-                            <h5 class="card-title">${conteudo.cep}</h5>
-                        </div>
-                        <ul class="list-group list-group-flush">
-                            <li class="list-group-item"><b>Logradouro:</b> ${conteudo.logradouro}</li>
-                            <li class="list-group-item"><b>Localidade:</b> ${conteudo.localidade}</li>
-                            <li class="list-group-item"><b>UF:</b> ${conteudo.uf}</li>
-                        </ul>
-                    </div><br>`
+                // document.getElementById('resposta').innerHTML =
+                //     `<div class="card" style="width: 18rem;">
+                //         <div class="card-body">
+                //             <h5 class="card-title">${conteudo.cep}</h5>
+                //         </div>
+                //         <ul class="list-group list-group-flush">
+                //             <li class="list-group-item"><b>Logradouro:</b> ${conteudo.logradouro}</li>
+                //             <li class="list-group-item"><b>Localidade:</b> ${conteudo.localidade}</li>
+                //             <li class="list-group-item"><b>UF:</b> ${conteudo.uf}</li>
+                //         </ul>
+                //     </div><br>`
             }
         })
     }
--- js/script2.js
+++ js/script2.js
@@ -1,10 +1,18 @@
-const pesquisarCep = async () => {
-    let cep = document.getElementById('cep').value;
+var estados = [];
+
+function compararEstados() {
+    let ceps = ['cep-origem', 'cep-destino']
+    ceps.forEach(pesquisarCep)
+    console.log(estados)
+}
+
+async function pesquisarCep(cep) {
+    let cepConsultado = document.getElementById(cep).value;
     var validacep = /^[0-9]{8}$/;
-    if (cep.trim() === "" || !validacep.test(cep)) {
+    if (!validacep.test(cepConsultado)) {
         document.getElementById('resposta').innerHTML = `<h5>Formato do CEP inválido</h5>`
     } else {
-        await fetch(`https://viacep.com.br/ws/${cep}/json/`, {
+        await fetch(`https://viacep.com.br/ws/${cepConsultado}/json/`, {
             method: 'GET',
             headers: {
                 'Content-Type': 'application/json'
@@ -18,20 +26,22 @@ const pesquisarCep = async () => {
             if ("erro" in conteudo) {
                 document.getElementById('resposta').innerHTML = `<h5>CEP não encontrado</h5>`
             } else {
-                document.getElementById('resposta').innerHTML = 
-                `<div class="card" style="width: 18rem;">                
-                <div class="card-body">
-                  <h5 class="card-title">${conteudo.cep}</h5>
-                </div>
-                <ul class="list-group list-group-flush">
-                    <li class="list-group-item">Lougradouro: ${conteudo.logradouro}</li>
-                    <li class="list-group-item">Localidade: ${conteudo.localidade}</li>
-                    <li class="list-group-item">UF: ${conteudo.uf}</li>
-                    <li class="list-group-item">A TruckLog atende na sua região</li>
-                </ul>                  
-                </div>
-              </div> <hr/>`
+                estados.unshift(conteudo.uf)
+            //     document.getElementById('resposta').innerHTML =
+            //         `<div class="card" style="width: 18rem;">                
+            //     <div class="card-body">
+            //       <h5 class="card-title">${conteudo.cep}</h5>
+            //     </div>
+            //     <ul class="list-group list-group-flush">
+            //         <li class="list-group-item">Lougradouro: ${conteudo.logradouro}</li>
+            //         <li class="list-group-item">Localidade: ${conteudo.localidade}</li>
+            //         <li class="list-group-item">UF: ${conteudo.uf}</li>
+            //         <li class="list-group-item">A TruckLog atende na sua região</li>
+            //     </ul>                  
+            //     </div>
+            //   </div> <hr/>`
             }
         })
     }
-};
+}
+
